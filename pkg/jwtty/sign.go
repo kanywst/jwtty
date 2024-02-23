204e076b52fc7f06243e35bb72ba6a5610ce5523
kanywst <niwatakuma@icloud.com>
2024-02-23 22:37:43 +0900

    Refactor

--- pkg/jwtty/sign.go
+++ pkg/jwtty/sign.go
@@ -1,80 +1,89 @@
 package jwtty
 
 import (
-	"crypto/rsa"
 	"crypto/x509"
 	"encoding/pem"
 	"errors"
 	"fmt"
-	"io/ioutil"
+	"io"
+	"os"
 
 	"github.com/golang-jwt/jwt/v5"
 )
 
 func SignWithRSA(c jwt.MapClaims, p string) (string, error) {
-	privateKeyBytes, err := ioutil.ReadFile(p)
+	key, err := parsePrivateKey(p)
 	if err != nil {
 		return "", err
-	}
-
-	block, _ := pem.Decode(privateKeyBytes)
-	if block == nil {
-		return "", errors.New("failed to decode PEM block containing private key")
-	}
-
-	key, err := x509.ParsePKCS8PrivateKey(block.Bytes)
-	if err != nil {
-		return "", err
-	}
-
-	rsaPrivateKey, ok := key.(*rsa.PrivateKey)
-	if !ok {
-		return "", errors.New("failed to parse RSA private key")
 	}
 
 	token := jwt.NewWithClaims(jwt.SigningMethodRS256, c)
-
-	signedToken, err := token.SignedString(rsaPrivateKey)
+	signedToken, err := sign(token, key)
 	if err != nil {
 		return "", err
 	}
-
 	return signedToken, nil
 }
 
 func SignWithECDSA(c jwt.MapClaims, p string) (string, error) {
-	privateKeyBytes, err := ioutil.ReadFile(p)
+	key, err := parsePrivateKey(p)
 	if err != nil {
 		return "", err
 	}
 
-	block, _ := pem.Decode(privateKeyBytes)
-	if block == nil {
-		return "", errors.New("failed to decode PEM block containing private key")
-	}
-
-	var key interface{}
-	switch block.Type {
-	case "EC PRIVATE KEY":
-		key, err = x509.ParseECPrivateKey(block.Bytes)
-		if err != nil {
-			return "", fmt.Errorf("failed to parse EC private key: %v", err)
-		}
-	case "PRIVATE KEY":
-		key, err = x509.ParsePKCS8PrivateKey(block.Bytes)
-		if err != nil {
-			return "", fmt.Errorf("failed to parse PKCS8 private key: %v", err)
-		}
-	default:
-		return "", errors.New("unsupported private key type")
-	}
-
 	token := jwt.NewWithClaims(jwt.SigningMethodES256, c)
+	signedToken, err := sign(token, key)
+	if err != nil {
+		return "", err
+	}
+	return signedToken, nil
+}
 
+func sign(token *jwt.Token, key interface{}) (string, error) {
 	signedToken, err := token.SignedString(key)
 	if err != nil {
 		return "", fmt.Errorf("failed to sign token: %v", err)
 	}
 
 	return signedToken, nil
+}
+
+func parsePrivateKey(p string) (interface{}, error) {
+	file, err := os.Open(p)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	privateKeyBytes, err := io.ReadAll(file)
+	if err != nil {
+		return nil, err
+	}
+
+	block, _ := pem.Decode(privateKeyBytes)
+	if block == nil {
+		return nil, errors.New("failed to decode PEM block containing private key")
+	}
+
+	var key interface{}
+	switch block.Type {
+	case "RSA PRIVATE KEY":
+		key, err = x509.ParsePKCS1PrivateKey(block.Bytes)
+		if err != nil {
+			return nil, err
+		}
+	case "EC PRIVATE KEY":
+		key, err = x509.ParseECPrivateKey(block.Bytes)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse EC private key: %v", err)
+		}
+	case "PRIVATE KEY":
+		key, err = x509.ParsePKCS8PrivateKey(block.Bytes)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse PKCS8 private key: %v", err)
+		}
+	default:
+		return nil, errors.New("unsupported private key type")
+	}
+	return key, nil
 }
